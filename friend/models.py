3dd32b3f23833117d76dded2d52e92e4b6ae8d2f
faizan1487 <faizanahmed14887@gmail.com>
2024-04-27 13:12:13 +0500

    friend request model created

--- friend/models.py
+++ friend/models.py
@@ -51,13 +51,60 @@ class FriendList(models.Model):
 
 
 
-# class FriendRequest(models.Model):
-#     """
-#     A friend request consists of two main parts:
-#         1. SENDER
-#             - Person sending the friend request
-#         2.  RECEIVER
-#             - Person receiving the friend request
-#     """
+class FriendRequest(models.Model):
+    """
+    A friend request consists of two main parts:
+        1. SENDER
+            - Person sending the friend request
+        2.  RECEIVER
+            - Person receiving the friend request
+    """
 
-#     sender = models.ForeignKey()
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receiver")
+
+    is_active = models.BooleanField(blank=True, null=False, default=True)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.sender.username
+
+
+    def accept(self):
+        """
+        Accept a friend request
+        update both sender and receiver friend lists
+        """
+        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        if receiver_friend_list:
+            receiver_friend_list.add_friend(self.sender)
+            sender_friend_list = FriendList.objects.get(user=self.sender)
+            if sender_friend_list:
+                sender_friend_list.add_friend(self.receiver)
+                self.is_active = False
+                self.save()
+
+
+    def decline(self):
+        """
+        Decline a friend request
+        It is "declined" by setting the 'is_active' field to False
+        """
+
+        self.is_active = False
+        self.save()
+
+
+    def cancel(self):
+        """
+        Cancel a friend request
+        It is 'cancelled' by etting the 'is_active' field to False
+        This is only different with respect to "declining" through the notification that
+        is generated.
+        """
+        self.is_active = False
+        self.save()
+
+        
