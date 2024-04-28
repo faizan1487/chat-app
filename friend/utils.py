5601f7b736c9c50a1683a1ec9d92e9e63011fa75
faizan1487 <faizanahmed14887@gmail.com>
2024-04-28 11:07:43 +0500

    friend srequest status conditions implemented

--- friend/utils.py
+++ friend/utils.py
@@ -2,7 +2,7 @@ from friend.models import FriendRequest
 
 def get_friend_request_or_false(sender, receiver):
     try:
-        return FriendRequest.object.get(sender=sender, receiver=receiver, is_active=True)
+        return FriendRequest.objects.get(sender=sender, receiver=receiver, is_active=True)
     except FriendRequest.DoesNotExist:
         return False
 
